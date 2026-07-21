16132154a0af2f418e85c433898dc182a999aa49
kat <katwang7749@gmail.com>
2026-07-20 23:16:01 -0400

    add hero text and some other stuff

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<div class="bg-linear-to-b from-cream to-white">
+	<!-- header -->
+	<div
+		class="sticky mx-8 mt-4 flex items-center justify-between bg-cream/75 text-[24px] backdrop-blur-md"
+	>
+		<div class="flex flex-row gap-6 text-dark-blue">
+			<h2>home</h2>
+			<h2>blog</h2>
+			<h2>etc</h2>
+		</div>
+		<h2 class="text-teal">graphic design is my passion!!!!!!</h2>
+	</div>
+	<!-- main -->
+	<div>
+		<!-- hero -->
+		<div class="flex justify-center m-16">
+			<div class="flex flex-col w-5/18">
+				<p class="font-bold text-[24px] text-red -mb-2">DICTIONARY</p>
+                <div class="flex flex-row items-baseline gap-4">
+					<h1 class="text-[64px] text-dark-blue">kat wang</h1>
+					<p class="text-[20px] text-dark-blue/50 italic">/kˈat wˈaŋɡ/</p>
+				</div>
+                <p class="text-dark-blue text-[24px] leading-[28px]">
+                    1. someone who likes to make games, websites, and illustrations
+                    <br/>2. most intelligent <a class="bg-clip-text text-transparent bg-linear-to-r from-blue to-dark-pink underline hover:decoration-wavy" href="https://hackclub.com" target="_blank">hack clubber</a>
+                </p> 
+                <!-- make this a component later -->
+                <div class="gap-3 my-4 flex flex-row">
+                    <a href="https://instagram.com/777luckykat"><img src="/imgs/contact-ig.png" alt="link to my instagram"/></a>
+                    <a href="https://instagram.com/777luckykat"><img src="/imgs/contact-mail.png" alt="link to my instagram"/></a>
+                    <a href="https://instagram.com/777luckykat"><img src="/imgs/contact-gh.png" alt="link to my instagram"/></a>
+                    <a href="https://instagram.com/777luckykat"><img src="/imgs/contact-slack.png" alt="link to my instagram"/></a>                                                            
+                </div> 
+			</div>
+		</div>
+	</div>
+</div>
