4d0e82a60498a45a83c60d1aad1c2bfeb6fca80a
kat <katwang7749@gmail.com>
2026-07-29 16:51:25 -0400

    add songs to music page

--- src/routes/more/music/+page.svelte
+++ src/routes/more/music/+page.svelte
@@ -1,12 +1,96 @@
-<main class="bg-light-blue p-12 flex flex-col">
+<script lang="ts">
+</script>
+
+<main class="flex flex-col bg-linear-to-b from-cream to-light-blue p-12">
 	<a class="mb-6 text-xl text-dark-pink italic underline hover:decoration-wavy" href="/more"
 		>← back</a
 	>
-	<h1 class="text-5xl text-blue italic">
+	<h1 class="text-5xl text-blue italic font-bold">
 		when im with U it feels like muuuuuuuuuusiiiiiiiiiiccccccccccccccccccccc
 	</h1>
 	<p class="text-2xl text-dark-blue/75">*a reference to the underscores song, Music</p>
-    <div>
-        <h2 class="text-4xl text-dark-blue">i like beabadoobee</h2>
-    </div>
+	<div class="flex flex-col gap-2">
+		<div class="music-container">
+			<h1 class="mb-4 text-2xl text-dark-blue italic">beabadoobee my favourite artist ever</h1>
+			<iframe
+				title="beabadoobee"
+				data-testid="embed-iframe"
+				style="border-radius:12px"
+				src="https://open.spotify.com/embed/artist/35l9BRT7MXmM8bv2WDQiyB?utm_source=generator"
+				width="100%"
+				height="152"
+				frameBorder="0"
+				allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+				loading="lazy"
+			></iframe>
+		</div>
+		<h2 class="self-center text-center text-dark-pink text-3xl m-4 italic">favourite songs rn</h2>
+		<div class="flex flex-wrap justify-center">
+			<div class="music-container-2">
+				<iframe
+					title="ever seen"
+					data-testid="embed-iframe"
+					style="border-radius:12px"
+					src="https://open.spotify.com/embed/track/505HhtdgN8lynQcJQCcIiD?utm_source=generator"
+					height="352"
+					width="100%"
+					frameBorder="0"
+					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					loading="lazy"
+				></iframe>
+			</div>
+			<div class="music-container-2">
+				<iframe
+					title="aint in la"
+					data-testid="embed-iframe"
+					style="border-radius:12px"
+					src="https://open.spotify.com/embed/track/02HyFYmpzt02VJ8k0CqxKj?utm_source=generator"
+					width="100%"
+					height="352"
+					frameBorder="0"
+					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					loading="lazy"
+				></iframe>
+			</div>
+			<div class="music-container-2">
+				<iframe
+					title="card declined"
+					data-testid="embed-iframe"
+					style="border-radius:12px"
+					src="https://open.spotify.com/embed/track/3vFRHL7DuN076fdIQuWQfv?utm_source=generator"
+					width="100%"
+					height="352"
+					frameBorder="0"
+					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					loading="lazy"
+				></iframe>
+			</div>
+			<div class="music-container-2">
+				<iframe
+					title="designated driver"
+					data-testid="embed-iframe"
+					style="border-radius:12px"
+					src="https://open.spotify.com/embed/track/3YJ6sHSqgzn5VLgRiViNtD?utm_source=generator"
+					width="100%"
+					height="352"
+					frameBorder="0"
+					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					loading="lazy"
+				></iframe>
+			</div>
+			<div class="music-container-2">
+				<iframe
+					title="house that always rains"
+					data-testid="embed-iframe"
+					style="border-radius:12px"
+					src="https://open.spotify.com/embed/track/1b6l3C0O4BGe0SivPwfoRH?utm_source=generator"
+					width="100%"
+					height="352"
+					frameBorder="0"
+					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					loading="lazy"
+				></iframe>
+			</div>
+		</div>
+	</div>
 </main>
