dd576e05ca045ef6d0b6a164e163bb7d7bda3fad
kat <katwang7749@gmail.com>
2026-07-29 19:31:36 -0400

    add more drawings and some pictures to landing page

--- src/routes/more/drawings/+page.svelte
+++ src/routes/more/drawings/+page.svelte
@@ -8,7 +8,7 @@
 	>
     <h2 class="text-3xl md:text-4xl lg:text-5xl text-dark-pink m-4 p-4 text-center">my drawings & design work (in no particular order)
     </h2>
-    <div class="flex flex-wrap gap-6 justify-center items-center self-center">
+    <div class="m-12 flex flex-wrap h-full gap-6 justify-center items-center self-center">
         {#each drawings as drawing}
             <img src={drawing} alt="" class="w-100 outline-blue/50 outline-1">
         {/each}
