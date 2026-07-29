f25b1248a9e05733c28e10748c505ee210642399
kat <katwang7749@gmail.com>
2026-07-29 19:35:46 -0400

    fix div width on /drawings

--- src/routes/more/drawings/+page.svelte
+++ src/routes/more/drawings/+page.svelte
@@ -2,13 +2,13 @@
     let drawings = Object.keys(import.meta.glob('$lib/assets/drawings/*'))
 </script>
 
-<div>
+<div class="flex flex-col">
 	<a class="mx-12 my-6 text-xl text-teal italic underline hover:decoration-wavy" href="/more"
 		>← back</a
 	>
     <h2 class="text-3xl md:text-4xl lg:text-5xl text-dark-pink m-4 p-4 text-center">my drawings & design work (in no particular order)
     </h2>
-    <div class="m-12 flex flex-wrap h-full gap-6 justify-center items-center self-center">
+    <div class="w-full xl:w-2/3 m-12 flex flex-wrap h-full gap-6 justify-center items-center self-center">
         {#each drawings as drawing}
             <img src={drawing} alt="" class="w-100 outline-blue/50 outline-1">
         {/each}
